ca7d69df186104dd61dbac15842ca64158c58b8c
Shishir <shishir16subedi@gmail.com>
2025-08-05 19:27:15 +0545

    Feat(app): added rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,33 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './user/user.module';
 import { AppConfigModule } from './common/config/config.module';
 import { DatabaseModule } from './database/database.module';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtmoduleModule } from './common/jwtmodule/jwtmodule.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        throttlers: [
+          {
+            name: 'default',
+            ttl: configService.get<number>('RATE_LIMIT_TTL', 60000),
+            limit: configService.get<number>('RATE_LIMIT_MAX', 70),
+          },
+        ],
+      }),
+    }),
     DatabaseModule, UsersModule, AuthModule, AppConfigModule, JwtmoduleModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard, // add throttler guard globally
+    },
+  ],
 })
 export class AppModule { }
