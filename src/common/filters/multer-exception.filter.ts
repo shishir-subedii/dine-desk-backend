4f103083a9c68fcce9bf57c2b1dc5bc493e60583
Shishir <shishir16subedi@gmail.com>
2025-08-28 18:23:58 +0545

    Feat(file-upload): added file upload module

---
+++ src/common/filters/multer-exception.filter.ts
@@ -0,0 +1,33 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    BadRequestException,
+} from '@nestjs/common';
+import { MulterError } from 'multer';
+
+@Catch(MulterError)
+export class MulterExceptionFilter implements ExceptionFilter {
+    catch(exception: MulterError, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse();
+
+        if (exception.code === 'LIMIT_FILE_SIZE') {
+            return response.status(400).json({
+                success: false,
+                statusCode: 400,
+                message: 'File too large. Max allowed size is 5MB.',
+                data: null
+            });
+        }
+
+        return response.status(400).json({
+            success: false,
+            statusCode: 400,
+            message: exception.message || 'Invalid file upload.',
+            data: null
+        });
+    }
+}
+
+//@UseFilters(MulterExceptionFilter)
