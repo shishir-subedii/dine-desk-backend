e6e125e3bdbc752ec280d1e3137c2070241f1dea
Shishir <shishir16subedi@gmail.com>
2025-09-13 14:11:12 +0545

    Fix(branch): Removed branch

--- src/database/persistence/persistence.module.ts
+++ src/database/persistence/persistence.module.ts
@@ -2,9 +2,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Application } from 'src/application/entities/application.entity';
-import { Branch } from 'src/branch/entities/branch.entity';
 import { Delivery } from 'src/delivery/entities/delivery.entity';
-import { GenericMenu } from 'src/menu/entities/generic_menu.entity';
 import { MenuItem } from 'src/menu/entities/menu.entity';
 import { OrderItem } from 'src/order/entities/order-item.entity';
 import { Order } from 'src/order/entities/order.entity';
@@ -18,15 +16,13 @@ import { Voucher } from 'src/voucher/entities/voucher.entity';
         TypeOrmModule.forFeature([
             User,
             Restaurant,
-            Branch,
             MenuItem,
             Order,
             OrderItem,
             Delivery,
             Voucher, 
             Payment,
-            Application,
-            GenericMenu,
+            Application
         ]),
     ],
     exports: [TypeOrmModule], // Export so other modules can inject repositories
